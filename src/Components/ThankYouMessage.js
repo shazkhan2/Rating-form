867de8d6dd034dd46e471dc859c7f01773582673
shazkhan2 <seewardak@hotmail.com>
2024-03-10 09:58:16 +0100

    styling fixed

--- src/Components/ThankYouMessage.js
+++ src/Components/ThankYouMessage.js
@@ -5,8 +5,9 @@ export default function ThankYouMessage({ selectedRating }) {
   return (
     <div className="thank-you-message">
       <img src="/illustration-thank-you.svg" alt="Thank You Illustration" className="thank-you-illustration" />
-      <h1>Thank you for your feedback!</h1>
-      <p>You rated us: {selectedRating} out of 5.</p>
+      <p id="rate">You rated us: {selectedRating} out of 5.</p>
+      <h1>Thank you</h1>
+      <p>We appreciate you taking the time to give us a rating.<br/> If you ever need more support, don't hesitate <br/>to get in touch!</p>
     </div>
   );
 }
