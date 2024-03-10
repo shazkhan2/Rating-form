7e7756e8b50d2c0f9a0ff024d97045e0c198a5c3
shazkhan2 <seewardak@hotmail.com>
2024-03-10 10:57:57 +0100

    Typo fixed

--- src/Components/ThankYouMessage.js
+++ src/Components/ThankYouMessage.js
@@ -7,7 +7,7 @@ export default function ThankYouMessage({ selectedRating }) {
       <img src="/illustration-thank-you.svg" alt="Thank You Illustration" className="thank-you-illustration" />
       <p id="rate">You rated us: {selectedRating} out of 5.</p>
       <h1>Thank you</h1>
-      <p>We appreciated you taking the time to give a rating.<br/> If you ever need more support. Don't hesitate <br/>to get in touch!</p>
+      <p>We appreciate you taking the time to give us a rating.<br/> If you ever need more support, don't hesitate <br/>to get in touch!</p>
     </div>
   );
 }
